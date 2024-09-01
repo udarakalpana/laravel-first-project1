d6f45d2c99de2d365ac4c969db7ae185ffcf2f5e
kalpana <kalpanaudara058@gmail.com>
2024-09-01 11:01:45 +0530

    Created user migration and model

---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password'
+    ];
+}
